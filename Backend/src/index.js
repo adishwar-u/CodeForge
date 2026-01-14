8d814af63549c7a785f3feaa578bd00339bdd2a0
Judas-u <adishwarraj9905@gmail.com>
2026-01-14 10:03:05 +0530

    Server-Ready

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,16 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 require('dotenv').config();
+const main = require('./config/db');
+const cookieParser = require('cookie-parser');
 
-app.listen(process.env.PORT, ()=>{
-    console.log("Server Listening at port numeber : " + process.env.PORT);
-} )
+app.use(express());
+app.use(cookieParser());
+
+main()
+.then(async () =>{
+    app.listen(process.env.PORT, ()=>{
+        console.log("Server Listening at port numeber : " + process.env.PORT);
+    })
+})
+.catch(err => console.log("Error Occured: " + err));
